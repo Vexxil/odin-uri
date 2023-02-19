42b47b8a670c98a7983aa9e5fb95d4ec1cb107c3
Dallas Phillips <dallasphillips24@gmail.com>
2023-02-18 20:29:59 -0800

    added suppoer for ipv4 and host

--- uri.go
+++ uri.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"errors"
 	"fmt"
+	"strconv"
 	"strings"
 )
 
@@ -11,6 +12,7 @@ const genDelims = ":/?#[]@"
 const subDelims = "!$&'()*+,;="
 const alpha = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
 const digit = "0123456789"
+const pctEncoding = digit + "abcdefABCDEF"
 
 type URI interface {
 	Schema() string
@@ -75,12 +77,20 @@ func parseAuthority(runes []rune) (string, error) {
 	if string(runes[0:2]) != "//" {
 		return "", errors.New("no authority")
 	}
-	// TODO do proper parse
-	for i, r := range runes[2:] {
-		if r == '/' {
-			return string(runes[2 : i+2]), nil
+	runes = runes[2:]
+	authority := ""
+	if userInfo, i, uiErr := parseUserInfo(runes); uiErr != nil {
+		if runes[3] != '@' {
+			return "", errors.New(fmt.Sprintf("invalid authority: %s", uiErr.Error()))
 		}
+		authority = userInfo + "@"
+		runes = runes[i:]
 	}
+	host, hostErr := parseHost(runes)
+	if hostErr == nil {
+		authority = authority + host
+	}
+
 	return "", errors.New("invalid authority")
 }
 
@@ -88,36 +98,174 @@ func parsePath(runes []rune) (string, error) {
 	panic("todo")
 }
 
-func parseUserInfo() {
-
+func parseUserInfo(runes []rune) (string, int, error) {
+	userInfo := make([]rune, 0)
+	rl := len(runes)
+	i := 0
+	for i < rl {
+		r := runes[i]
+		if r == ':' {
+			userInfo = append(userInfo, ':')
+		} else if isSubDelim(r) || !isReserved(r) {
+			userInfo = append(userInfo, r)
+		} else {
+			pctEncoded, pctErr := parsePctEncoded(runes[i:])
+			if pctErr != nil {
+				return "", -1, errors.New("invalid user-info")
+			}
+			userInfo = append(userInfo, pctEncoded...)
+			i += 3
+			continue
+		}
+		i++
+	}
+	return string(userInfo), i, nil
 }
 
-func parseHost() {
+func parseHost(runes []rune) (string, error) {
+	ipLit, ipLitErr := parseIpLiteral(runes)
+	if ipLitErr == nil {
+		return ipLit, nil
+	}
+	ipv4, _, ipv4Err := parseIpv4(runes)
+	if ipv4Err == nil {
+		return ipv4, nil
+	}
+	regHost, rhErr := parseRegHost(runes)
+	if rhErr == nil {
+		return regHost, nil
+	}
+	return "", errors.New("invalid host")
+}
 
+func parseRegHost(runes []rune) (string, error) {
+	if len(runes) == 0 {
+		return "", errors.New("no host name")
+	}
+	regHost := make([]rune, 0)
+	i := 0
+	for i < len(runes) {
+		r := runes[i]
+		i++
+		if !isReserved(r) || isSubDelim(r) {
+			regHost = append(regHost, r)
+		} else {
+			pctEncoded, pctErr := parsePctEncoded(runes[i : i+2])
+			if pctErr == nil {
+				// TODO is this right?
+				continue
+			} else {
+				regHost = append(regHost, pctEncoded...)
+			}
+		}
+	}
+	if len(regHost) == 0 {
+		return "", errors.New("no host name")
+	}
+	return string(regHost), nil
 }
 
 func parsePort() {
 
 }
 
-func parseIpLiteral() {
-
+func parseIpLiteral(runes []rune) (string, error) {
+	if len(runes) == 0 {
+		return "", errors.New("no ip literal")
+	}
+	if runes[0] != '[' {
+		return "", errors.New("invalid ip literal: missing '['")
+	}
+	ipv6, end, ipv6Err := parseIpv4(runes[1:])
+	if ipv6Err == nil && runes[end-1] == ']' {
+		return "[" + ipv6 + "]", nil
+	}
+	ipvf, end, ipvfErr := parseIpvFuture(runes[1:])
+	if ipvfErr == nil && runes[end-1] == ']' {
+		return "[" + ipvf + "]", nil
+	}
+	return "", errors.New("invalid ip-literal")
 }
 
-func parseIpv4() {
+func parseIpv4(runes []rune) (string, int, error) {
+	if len(runes) == 0 {
+		return "", -1, errors.New("no ipv4")
+	}
 
+	ipv4 := make([]rune, 0)
+	octet := make([]rune, 0)
+	count := 0
+	octetStart := false
+	end := 0
+	for i, r := range runes {
+		end = i
+		if r == '.' {
+			ipv4 = append(ipv4, octet...)
+			ipv4 = append(ipv4, r)
+			count = count + 1
+			octet = make([]rune, 0)
+			continue
+		} else {
+			if !isDigit(r) {
+				return "", -1, errors.New(fmt.Sprintf("invalid ip literal: invalid character %c", r))
+			}
+			if octetStart {
+				octetLen := len(octet)
+				if octetLen == 3 {
+					return "", -1, errors.New("invalid ip literal: octet length")
+				}
+				octet = append(octet, r)
+				parseInt, err := strconv.ParseInt(string(octet), 10, 32)
+				if err != nil {
+					return "", -1, errors.New(fmt.Sprintf("invalid ip literal: invalid octet %s", string(octet)))
+				}
+				if parseInt > 255 {
+					return "", -1, errors.New(fmt.Sprintf("invalid ip literal: invalid octet %d", parseInt))
+				}
+			} else {
+				if r == '1' || r == '2' {
+					octet = append(octet, r)
+				} else {
+					return "", -1, errors.New("invalid ip literal")
+				}
+				octetStart = true
+			}
+		}
+		if count == 4 {
+			break
+		}
+	}
+	return string(ipv4), end, nil
 }
 
-func parseIpv6() {
-
+func parseIpv6(runes []rune) (string, int, error) {
+	panic("not implemented")
 }
 
-func parseIpvFuture() {
-
+func parseIpvFuture(runes []rune) (string, int, error) {
+	panic("")
 }
 
-func parsePctEncoded() {
+func parsePctEncoded(runes []rune) ([]rune, error) {
+	var res []rune
+	if len(runes) == 0 {
+		return res, errors.New("no pct-encoding")
+	}
+	if runes[0] != '%' {
+		return res, errors.New("invalid pct-encoding: missing '%'")
+	}
+	if len(runes) < 3 {
+		return res, errors.New("invalid pct-encoding: too short")
+	}
+	res = append(res, '%')
+	for _, r := range runes[1:2] {
+		if !strings.ContainsRune(pctEncoding, r) {
+			return []rune{}, errors.New(fmt.Sprintf("invalid pct-encoding: %c is not in range", r))
+		}
+		res = append(res, r)
+	}
 
+	return res, nil
 }
 
 func runeIndex(runes []rune, value rune) int {
@@ -127,6 +275,10 @@ func runeIndex(runes []rune, value rune) int {
 		}
 	}
 	return -1
+}
+
+func isSubDelim(value rune) bool {
+	return strings.ContainsRune(subDelims, value)
 }
 
 func isReserved(value rune) bool {
--- uri_test.go
+++ uri_test.go
@@ -1,6 +1,9 @@
 package odin_uri
 
-import "testing"
+import (
+	"bytes"
+	"testing"
+)
 
 func TestParseUriHttp(t *testing.T) {
 	var httpUri = "http://www.ietf.org/rfc/rfc2396.txt"
@@ -13,5 +16,52 @@ func TestParseUriHttp(t *testing.T) {
 	}
 	if uri.Authority() != "www.ietf.org" {
 		t.Fatalf("invalid http authority: %s", uri.Authority())
+	}
+}
+
+func TestParsePctEncoding(t *testing.T) {
+	runes := []rune{'%', '1', 'f'}
+	_, err := parsePctEncoded(runes)
+	if err != nil {
+		t.Fatal(err)
+		return
+	}
+	badRunes := []rune{'%', 'g', '3'}
+	_, err = parsePctEncoded(badRunes)
+	if err == nil {
+		t.Fatalf("%s", "'%g3' should fail as pct encoding")
+	}
+}
+
+func TestParseIpv4(t *testing.T) {
+	runes := bytes.Runes([]byte("192.168.0.1"))
+	_, _, err := parseIpv4(runes)
+	if err != nil {
+		t.Fatal(err)
+		return
+	}
+	runes = bytes.Runes([]byte("111.111.111.111"))
+	_, _, err = parseIpv4(runes)
+	if err != nil {
+		t.Fatal(err)
+		return
+	}
+}
+
+func TestParseHost(t *testing.T) {
+	runes := bytes.Runes([]byte("123.12.12.123"))
+	host, err := parseHost(runes)
+	if err != nil {
+		t.Fatal(err)
+		return
+	}
+	runes = bytes.Runes([]byte("testhost"))
+	host, err = parseHost(runes)
+	if err != nil {
+		t.Fatal(err)
+		return
+	}
+	if host != "testhost" {
+		t.Fatalf("%s != %s", host, "testhost")
 	}
 }
